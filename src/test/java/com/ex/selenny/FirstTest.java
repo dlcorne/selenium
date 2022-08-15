129ca36561b61801dc5a9b7e022d85428a4699af
Daniel Cornell <d.corne@hotmail.com>
2022-08-15 12:47:53 +0100

    more progress :]

--- src/test/java/com/ex/selenny/FirstTest.java
+++ src/test/java/com/ex/selenny/FirstTest.java
@@ -37,7 +37,6 @@ public class FirstTest {
 		
 		WebElement topResultAtc = driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div > div.right-block.col-xs-4.col-xs-12.col-md-4 > div > div.button-container.col-xs-7.col-md-12 > a.button.ajax_add_to_cart_button.btn.btn-default"));
 		topResultAtc.click();
-		// insert a delay here i think
 		WebElement checkout = driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a"));
 		checkout.click();
 		WebElement checkout2 = driver.findElement(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium"));
@@ -47,15 +46,55 @@ public class FirstTest {
 		emailBar.sendKeys(email);
 		emailBar.submit();
 		
-		// currently working on plugging these into the account page
+		// account page
 		String fname = "Bruh";
 		String lname = "Bruhson";
 		String pwd = "passwordlol";
 		String addy = "1738 Fetty Ave";
 		String city = "Bruhsville";
-		String zip = "what even are these";
-		String mob = "0121-DO-ONE";
+		String zip = "80084";
+		String mob = "01234567890";
 		String addyAlias = "Secret Bunker";
+		WebElement fnameBar = driver.findElement(By.cssSelector("#customer_firstname"));
+		fnameBar.sendKeys(fname);
+		WebElement lnameBar = driver.findElement(By.cssSelector("#customer_lastname"));
+		lnameBar.sendKeys(lname);
+		WebElement pwdBar = driver.findElement(By.cssSelector("#passwd"));
+		pwdBar.sendKeys(pwd);
+		WebElement addyBar = driver.findElement(By.cssSelector("#address1"));
+		addyBar.sendKeys(addy);
+		WebElement cityBar = driver.findElement(By.cssSelector("#city"));
+		cityBar.sendKeys(city);
+		
+		// maybe will not work
+		WebElement stateBar = driver.findElement(By.cssSelector("#id_state"));
+		stateBar.click();
+		stateBar.sendKeys("a");
+		stateBar.submit();
+		
+		WebElement zipBar = driver.findElement(By.cssSelector("#postcode"));
+		zipBar.sendKeys(zip);
+		WebElement mobBar = driver.findElement(By.cssSelector("#phone_mobile"));
+		mobBar.sendKeys(mob);
+		WebElement aliasBar = driver.findElement(By.cssSelector("#alias"));
+		aliasBar.sendKeys(addyAlias);
+		
+		// address page
+		WebElement checkout3 = driver.findElement(By.cssSelector("#center_column > form > p > button"));
+		checkout3.click();
+		
+		// shipping page
+		WebElement tos = driver.findElement(By.cssSelector("#cgv"));
+		tos.click();
+		WebElement checkout4 = driver.findElement(By.cssSelector("#form > p > button"));
+		checkout4.click();
+		
+		// payment page
+		WebElement wire = driver.findElement(By.cssSelector("#HOOK_PAYMENT > div:nth-child(1) > div > p > a"));
+		wire.click();
+		WebElement checkout5 = driver.findElement(By.cssSelector("#cart_navigation > button"));
+		checkout5.click();
+		
 	}
 	
 	@AfterEach
